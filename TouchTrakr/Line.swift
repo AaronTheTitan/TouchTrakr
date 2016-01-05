514fc498cc346ed55b8541041b632549c20b7d67
Aaron Bradley <abeats20@gmail.com>
2016-01-04 22:27:48 -0600

    setup initial line struct and uiview subclass

---
+++ TouchTrakr/Line.swift
@@ -0,0 +1,15 @@
+//
+//  Line.swift
+//  TouchTrakr
+//
+//  Created by Aaron Bradley on 1/4/16.
+//  Copyright © 2016 AaronTheTitan. All rights reserved.
+//
+
+import Foundation
+import CoreGraphics
+
+struct Line {
+  var begin = CGPoint.zero
+  var end = CGPoint.zero
+}
