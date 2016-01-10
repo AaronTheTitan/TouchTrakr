dcc2b2e9a6b6a9382c9054d51c7d310e91a24a89
aaronb <abeats20@gmail.com>
2016-01-10 12:35:24 -0600

    updated DrawView

--- TouchTrakr/DrawView.swift
+++ TouchTrakr/DrawView.swift
@@ -30,6 +30,7 @@ class DrawView: UIView {
   
   var currentLines = [NSValue: Line]()
   var finishedLines = [Line]()
+  var selectedLineIndex: Int?
   
   
   func strokeLine(line: Line) {
@@ -53,6 +54,59 @@ class DrawView: UIView {
     for (_, line) in currentLines {
       strokeLine(line)
     }
+    
+    if let index = selectedLineIndex {
+      UIColor.greenColor().setStroke()
+      let selectedLine = finishedLines[index]
+      strokeLine(selectedLine)
+    }
+  }
+  
+  required init?(coder aDecoder: NSCoder) {
+    super.init(coder: aDecoder)
+    
+    let doubleTapRecognizer = UITapGestureRecognizer(target: self, action: "doubleTap:")
+    doubleTapRecognizer.numberOfTapsRequired = 2
+    doubleTapRecognizer.delaysTouchesBegan = true
+    addGestureRecognizer(doubleTapRecognizer)
+    
+    let tapRecognizer = UITapGestureRecognizer(target: self, action: "tap:")
+    tapRecognizer.delaysTouchesBegan = true
+    tapRecognizer.requireGestureRecognizerToFail(doubleTapRecognizer)
+    addGestureRecognizer(tapRecognizer)
+  }
+  
+  func doubleTap(gestureRecognizer: UITapGestureRecognizer) {
+    print("Recognized a double tap")
+    
+    currentLines.removeAll(keepCapacity: false)
+    finishedLines.removeAll(keepCapacity: false)
+    setNeedsDisplay()
+  }
+  
+  func tap(gestureRecognizer: UITapGestureRecognizer) {
+    print("Recognized a tap")
+  }
+  
+  func indexOfLineAtPoint(point: CGPoint) -> Int? {
+    // find a line close to point
+    for (index, line) in finishedLines.enumerate() {
+      let begin = line.begin
+      let end = line.end
+      
+      // check a few points on the line
+      for t in CGFloat(0).stride(to: 1.0, by: 0.05) {
+        let x = begin.x + ((end.x - begin.x) * t)
+        let y = begin.y + ((end.y - begin.y) * t)
+        
+        // if the tapped point is within 20 points, let's return this line
+        if hypot(x - point.x, y - point.y) < 20.0 {
+          return index
+        }
+      }
+    }
+    // if nothing is close enough to the tapped point, then we did not select a line
+    return nil
   }
   
   override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
